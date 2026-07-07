98928e4a3ffb706876462c85b900b561c5c001fc
yuchen1204 <xiaoyuchen031204@gmail.com>
2026-07-08 00:59:35 +0800

    feat(void-game): add CSS styles for void proposal UI elements

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./AuthContext";
 import App from "./App";
 import "./styles/uno.css";
+import "./styles/void.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
